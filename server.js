2a9539001623a1084727673e7c5e3a8a44d7cab9
Alan Burga <ahbb21@hotmail.com>
2025-05-21 20:38:31 -0500

    first commit

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "docs")));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "docs", "index.html"));
 });
 
 app.listen(PORT, () => {
